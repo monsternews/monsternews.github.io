413ca1548f8618275774ecc3cf6a04205f676524
Monster News <monsternewschannel@gmail.com>
2018-04-23 20:49:48 -0400

    Commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "Commit"
-git remote add origin https://github.com/monsternews/monsternews.github.io.git
+#git remote add origin https://github.com/monsternews/monsternews.github.io.git
 git remote -v
 git push origin master
